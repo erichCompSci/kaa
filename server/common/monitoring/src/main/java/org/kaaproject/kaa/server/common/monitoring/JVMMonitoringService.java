c56507e035c950b43b9ade282c5e8b4a1ba64588
Igor Khanenko <ikhanenko@cybervisiontech.com>
2015-06-12 21:18:12 +0300

    KAA-307. Added property file for request throttling implementation.

--- server/common/monitoring/src/main/java/org/kaaproject/kaa/server/common/monitoring/JVMMonitoringService.java
+++ server/common/monitoring/src/main/java/org/kaaproject/kaa/server/common/monitoring/JVMMonitoringService.java
@@ -2,9 +2,13 @@ package org.kaaproject.kaa.server.common.monitoring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,72 +16,166 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static org.kaaproject.kaa.server.common.monitoring.MachineStateType.OVERLOADED;
+
+@Service
 public class JVMMonitoringService {
 
-    private static final int MB = 1024 * 1024;
-    /**
-     * The Constant DEFAULT_PRINT_STATISTIC_PERIOD.
-     */
-    private static final long DEFAULT_PRINT_STATISTIC_PERIOD = TimeUnit.SECONDS.toMillis(1);
-    /**
-     * The Constant DEFAULT_PRINT_STATISTIC_DELAY.
-     */
-    private static final long DEFAULT_PRINT_STATISTIC_DELAY = TimeUnit.SECONDS.toMillis(0);
     /**
      * The Constant LOG.
      */
     private static final Logger LOG = LoggerFactory.getLogger(JVMMonitoringService.class);
+    /**
+     * The Constant MB.
+     */
+    private static final int MB = 1024 * 1024;
 
-    private static final JVMMonitoringService instance = new JVMMonitoringService();
-    private static Map<String, TaskStatisticInfo> registered = new ConcurrentHashMap<>();
-    private static Runtime runtime = Runtime.getRuntime();
+    private Runtime runtime = Runtime.getRuntime();
+    private Map<String, TaskStatisticInfo> registered = new ConcurrentHashMap<>();
+    private List<GarbageCollectorMXBean> gcMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+    private OperatingSystemMXBean systemMXBean = ManagementFactory.getOperatingSystemMXBean();
 
     private ScheduledExecutorService checkStateScheduler = Executors.newScheduledThreadPool(1);
-    private List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
-    private static MachineStateType state = MachineStateType.NORMAL;
+    private ScheduledExecutorService recoveryStateScheduler = Executors.newScheduledThreadPool(1);
 
+    private long totalGCCount = 0;
+    private long totalGCTime = 0;
+
+    private MachineStateType state;
+
+    @Autowired
+    private MonitoringOptions options;
+
+    public JVMMonitoringService() {
+        state = MachineStateType.NORMAL;
+    }
+
+    @PostConstruct
     public void start() {
+        LOG.info("Starting JVM monitoring service...");
         checkStateScheduler.scheduleWithFixedDelay(new Runnable() {
             @Override
             public void run() {
-                printStatistics();
+                updateStatistics();
             }
-        }, DEFAULT_PRINT_STATISTIC_DELAY, DEFAULT_PRINT_STATISTIC_PERIOD, TimeUnit.MILLISECONDS);
+        }, options.getPrintStatisticsDelay(), options.getPrintStatisticPeriod(), TimeUnit.MILLISECONDS);
     }
 
     public void stop() {
+        LOG.info("Stop JVM monitoring service...");
         checkStateScheduler.shutdown();
         try {
             checkStateScheduler.awaitTermination(3, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-            LOG.error("", e);
+            LOG.error("Got error during stop check state scheduler.", e);
+        }
+        recoveryStateScheduler.shutdown();
+        try {
+            recoveryStateScheduler.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            LOG.error("Got error during stop recovery scheduler.", e);
         }
     }
 
 
-    private void printStatistics() {
-        for (Map.Entry<String, TaskStatisticInfo> entry : registered.entrySet()) {
-            String name = entry.getKey();
-            TaskStatisticInfo info = entry.getValue();
-            int input = info.getInputTasksCount();
-            int success = info.getSuccessTasksCount();
-            int failure = info.getFailureTasksCount();
-            if (input != 0 || success != 0 || failure != 0) {
-                LOG.info("{}: {}/{}/{} input/success/failure perf second", name, input, success, failure);
+    private void updateStatistics() {
+        calculate();
+        if (options.isPrintResourceUsageInfo()) {
+            for (Map.Entry<String, TaskStatisticInfo> entry : registered.entrySet()) {
+                String name = entry.getKey();
+                TaskStatisticInfo info = entry.getValue();
+                int input = info.getInputTasksCount();
+                int success = info.getSuccessTasksCount();
+                int failure = info.getFailureTasksCount();
+                if (input != 0 || success != 0 || failure != 0) {
+                    LOG.info("{}: {}/{}/{} input/success/failure perf second", name, input, success, failure);
+                }
             }
         }
     }
 
-    public static JVMMonitoringService getInstance(String name) {
-        return instance;
-    }
-
-    public static TaskStatisticInfo registerStatistics(String name, StateChangeCallback callback) {
+    public TaskStatisticInfo registerStatistics(String name, StateChangeCallback callback) {
+        LOG.info("Register statistics with name {}", name);
         TaskStatisticInfo statistic = new TaskStatisticInfo(callback);
         registered.put(name, statistic);
-        if (MachineStateType.OVERLOADED == state) {
+        if (OVERLOADED == state) {
             callback.onStateChange(state);
         }
         return statistic;
+    }
+
+    private void onChangeState() {
+        LOG.info("Execute on changing state callbacks.");
+        if (!registered.isEmpty()) {
+            for (Map.Entry<String, TaskStatisticInfo> entry : registered.entrySet()) {
+                entry.getValue().getCallback().onStateChange(state);
+            }
+        }
+    }
+
+    private void calculate() {
+        if (!gcMXBeans.isEmpty() && gcMXBeans.size() > 1) {
+            long max = toMB(runtime.maxMemory());
+            long total = toMB(runtime.totalMemory());
+            long free = toMB(runtime.freeMemory());
+            long used = total - free;
+            LOG.debug("Memory: {free: {}, max: {},  total: {}, used: {}}, LA: {}", free, max, total, used, systemMXBean.getSystemLoadAverage());
+            GarbageCollectorMXBean gcOldGen = gcMXBeans.get(1);
+            long gcTimeDelta = gcOldGen.getCollectionTime() - totalGCTime;
+            long gcCountDelta = gcOldGen.getCollectionCount() - totalGCCount;
+            totalGCTime += gcTimeDelta;
+            totalGCCount += gcCountDelta;
+            if (gcCountDelta >= 1 || getPendingTaskCount() >= options.getMaxPendingTaskCount()) {
+                if ((max == total) || (max - total) < (max * 0.1f)) {
+                    if (((float) (free) / (float) (max)) < options.getMinFreeMemory()) {
+                        if (gcCountDelta >= options.getFullGCCountDeltaLimit()) {
+                            LOG.warn("JVM is close to out of memory error. Sending notifications...");
+                            updateState(OVERLOADED);
+                        }
+                    }
+                }
+            }
+        } else {
+            LOG.warn("GC MX beans not found.");
+        }
+    }
+
+    private long toMB(long bytes) {
+        return bytes / MB;
+    }
+
+    private void updateState(MachineStateType currentState) {
+        if (OVERLOADED.equals(currentState) && !OVERLOADED.equals(state)) {
+            recoveryStateScheduler.schedule(new RecoveryCheckRunner(), options.getRejectRequestPeriod(), TimeUnit.MILLISECONDS);
+        }
+        this.state = currentState;
+        onChangeState();
+    }
+
+    private boolean isRecovered() {
+        boolean result = false;
+        if (getPendingTaskCount() <= options.getMinPendingTaskCount()) {
+            result = true;
+        }
+        return result;
+    }
+
+    private int getPendingTaskCount() {
+        int count = 0;
+        for (Map.Entry<String, TaskStatisticInfo> entry : registered.entrySet()) {
+            count += entry.getValue().getPendingTasksCount();
+        }
+        return count;
+    }
+
+    private class RecoveryCheckRunner implements Runnable {
+        @Override
+        public void run() {
+            if (isRecovered()) {
+                updateState(MachineStateType.NORMAL);
+            } else {
+                recoveryStateScheduler.schedule(new RecoveryCheckRunner(), options.getRejectRequestPeriod(), TimeUnit.MILLISECONDS);
+            }
+        }
     }
 }
