9390cb45d3df2ff3b97ffe45d31347e772e8b412
RostakaGmfun <rostawesomegd@gmail.com>
2015-08-10 16:59:23 +0300

    Fixed one more test

--- server/operations/src/test/java/org/kaaproject/kaa/server/operations/service/profile/ProfileServiceTest.java
+++ server/operations/src/test/java/org/kaaproject/kaa/server/operations/service/profile/ProfileServiceTest.java
@@ -74,7 +74,7 @@ public class ProfileServiceTest {
     public void testPopulateVersionStates() {
         EndpointProfileDto dtoMock = Mockito.mock(EndpointProfileDto.class);
 
-        SdkPropertiesDto sdkProperties = new SdkPropertiesDto(null, 1, 2, 3, 4, SdkPlatform.JAVA, Collections.EMPTY_LIST, null, null);
+        SdkPropertiesDto sdkProperties = new SdkPropertiesDto(null, 1, 2, 3, 4, SdkPlatform.JAVA, Collections.EMPTY_LIST, null, null, false);
 
         ApplicationEventFamilyMapDto applicationEventFamilyMap = new ApplicationEventFamilyMapDto();
         applicationEventFamilyMap.setVersion(7);
